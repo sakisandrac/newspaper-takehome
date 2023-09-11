40957e220cb0837682a7ccd9ad5eb0b90523c938
“Saki <sakisandra.c@gmail.com>
2023-09-11 10:33:22 -0600

    Add router and create components

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/'></Route>
+      </Routes>
+    </div>
+     
+  );
+}
+
+export default App;
